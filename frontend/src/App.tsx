7068c4ab191c470c6a04939922711a5e8bea8e42
Josh Clark <jclarktennis@gmail.com>
2026-01-22 15:32:44 -0500

    feat: add tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import "./App.css";
 import { UserProfile } from "./components/UserProfile";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app text-slate-900 bg-slate-500">
       <header className="app-header">
-        <h1>Monorepo Demo</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Monorepo Demo</h1>
         <p>This is a demo of a TypeScript monorepo with shared code between frontend and backend</p>
       </header>
-      <main>
+      <main className="w-full">
         <UserProfile />
       </main>
       <footer className="app-footer">
-        <p>TypeScript Monorepo Template</p>
+        <p className="text-sm text-slate-500">TypeScript Monorepo Template</p>
       </footer>
     </div>
   );
